48a952f357c1eaea4cb30cab53080c4930952aff
Aytaditya <adityaaryan531@gmail.com>
2024-07-30 02:10:51 +0530

    transaction details

--- workshop.js
+++ workshop.js
@@ -5,28 +5,30 @@ const neucron = new NeucronSDK();
 const authModule = neucron.authentication;
 const walletModule = neucron.wallet;
 
-const signUpResponse = await authModule.signUp({ email: "adityaaryan531@gmail.com", password: "aditya123" });
-console.log(signUpResponse);
+// const signUpResponse = await authModule.signUp({ email: "adityaaryan531@gmail.com", password: "aditya123" });
+// console.log(signUpResponse);
 
 const loginResponse = await authModule.login({ email: "sales@timechainlabs.io", password: "string" });
 console.log(loginResponse);
+
+// const loginResponse = await authModule.login({ email: "adityaaryan531@gmail.com", password: "aditya123" });
+// console.log(loginResponse);
 
 const walletKeys = await walletModule.getWalletKeys({});
 console.log(walletKeys);
 
 
 //For Default wallet balance
-
 const DefaultWalletBalance = await walletModule.getWalletBalance({});
 console.log(DefaultWalletBalance);
 
-// const addresses = await walletModule.getAddressesByWalletId({});
-// console.log(addresses);
+const addresses = await walletModule.getAddressesByWalletId({});
+console.log(addresses);
 
 const options = {
     outputs: [
       {
-        address: 'rohan@dev.neucron.io',
+        address: 'adityaaryan531@dev.neucron.io',
         note: 'gurudakshina',
         amount: 10
       }
@@ -36,25 +38,25 @@ const options = {
 const payResponse = await neucron.pay.txSpend(options)
 console.log(payResponse)
 
-const walletHistory = await walletModule.getWalletHistory({ });
-console.log(walletHistory);
+// const walletHistory = await walletModule.getWalletHistory({ });
+// console.log(walletHistory);
 
 
-console.log('initiating wallet')
-const walletCreation1 = await walletModule.createWallet({ walletName: 'Hello tsoc1' });
-console.log(walletCreation1);
+// console.log('initiating wallet')
+// const walletCreation1 = await walletModule.createWallet({ walletName: 'Hello tsoc1' });
+// console.log(walletCreation1);
 
-const walletBalance = await walletModule.getWalletBalance({ walletId: walletCreation1.walletID });
-console.log(walletBalance);
+// const walletBalance = await walletModule.getWalletBalance({ walletId: walletCreation1.walletID });
+// console.log(walletBalance);
 
-const addresses = await walletModule.getAddressesByWalletId({ walletId: walletCreation1.walletID });
-console.log(addresses);
+// const addresses = await walletModule.getAddressesByWalletId({ walletId: walletCreation1.walletID });
+// console.log(addresses);
 
-const mnemonic = await walletModule.getMnemonic({ walletId: walletCreation1.walletID });
-console.log(mnemonic);
+// const mnemonic = await walletModule.getMnemonic({ walletId: walletCreation1.walletID });
+// console.log(mnemonic);
 
-const allUtxos = await walletModule.getAllUtxos({ walletId: walletCreation1.walletID });
-console.log(allUtxos);
+// const allUtxos = await walletModule.getAllUtxos({ walletId: walletCreation1.walletID });
+// console.log(allUtxos);
 
-const xPubKeys = await walletModule.getXPubKeys({ walletId: walletCreation1.walletID });
-console.log(xPubKeys);
+// const xPubKeys = await walletModule.getXPubKeys({ walletId: walletCreation1.walletID });
+// console.log(xPubKeys);
